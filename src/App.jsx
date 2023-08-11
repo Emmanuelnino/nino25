fafa4eb49b900d4ab42935486b7a1e8f8769e09a
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-11 13:27:25 -0700

    change

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const App = () => {
   return(
     <div className="flex">
-      <div className={''}>Sidebar</div>
-      <div className="p-7 text-2xl font-semibold ">
+      <div className={'w-72 h-screen bg-dark-purple'}>Sidebar</div>
+      <div className="p-7 text-2xl font-semibold flex-1 h-screen">
         <h1>Home Page</h1>
       </div>
     </div>
