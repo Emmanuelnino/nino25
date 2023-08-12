e68a6a1b29cb48f3e8cf5082635d7edc314ea512
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-12 02:04:54 -0700

    change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className='bg-gray-100'>
-            <div className='flex max-w-7x1 mx-auto border border-red-400'>
+        <nav className='py-3 bg-gray-100'>
+            <div className='flex items-center justify-around max-w-7x1 mx-auto border border-gray-400'>
 
 
                 <div>logo</div>
