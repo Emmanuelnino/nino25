7ee769cc5183f64afbac43551aa6d5dc6df72122
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-12 00:36:39 -0700

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import React from "react";
+import React from 'react'
 
 const App = () => {
-  return(
-    <div className="flex">
-      <div className={'w-20 h-screen bg-dark-purple'}>Sidebar</div>
-      <div className="p-7 text-2xl font-semibold flex-1 h-screen">
-        <h1>Home Page</h1>
+  return (
+    <div>
+      <div>
+       
       </div>
     </div>
-    
+  )
+}
 
-  );
-  }
-
-export default App;
+export default App
