067d6b24383f62f7297406cbbb032581b622abf3
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-11 22:31:11 -0700

    change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return(
     <div className="flex">
-      <div className={'w-72 h-screen bg-dark-purple'}>Sidebar</div>
+      <div className={'w-20 h-screen bg-dark-purple'}>Sidebar</div>
       <div className="p-7 text-2xl font-semibold flex-1 h-screen">
         <h1>Home Page</h1>
       </div>
---
+++ src/sidebar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const sidebar = () => {
+    return (
+        <div>
+            <div>
+                <svg
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    height="1em"
+                    width="1em"
+                    
+                >
+                    <path d="M13.939 4.939L6.879 12l7.06 7.061 2.122-2.122L11.121 12l4.94-4.939z" />
+                </svg>
+            </div>
+        </div>
+    )
+}
+
+export default sidebar
