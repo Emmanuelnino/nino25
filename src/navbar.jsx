863612d586afc095b86b1a518aa5067fd1e48bc5
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-12 01:15:50 -0700

    change

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const navbar = () => {
     return (
         <nav>
-            <div className='max-w-7x1 mx-auto'>
+            <div className='max-w-7x1 mx-auto border '>
 
 
             </div>
