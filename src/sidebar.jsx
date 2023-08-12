a9439b2191a2b2f26eb937795df8b2c0dda569b8
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-11 23:25:06 -0700

    change

--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -4,7 +4,7 @@ const sidebar = () => {
     const [toggle,setToggle] = useState(false);
     return (
         <div className='{sidebar-container}'>
-            <div>
+            <div className='absolute-'>
                 <svg
                     viewBox="0 0 24 24"
                     height="2.5em"
