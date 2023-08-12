3209af78eee0f397834f4dfad4982eb9d682652c
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-12 02:14:23 -0700

    change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const Navbar = () => {
     return (
         <nav className='py-3 bg-gray-100'>
-            <div className='flex items-center justify-around max-w-7x1 mx-auto border border-gray-400'>
+            <div className='flex items-center justify-between max-w-7x1 mx-auto border border-gray-400'>
 
-
+<div className='flex gap-2'>
                 <div>logo</div>
 
 
 
                 <div>primary nav</div>
-
+                </div>
 
                 <div>secondary nav</div>  
 
