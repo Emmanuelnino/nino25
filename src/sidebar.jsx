823df42d9b7eebd01f4abfe27852ce4e97862b9f
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-11 22:32:57 -0700

    change

--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -7,7 +7,7 @@ const sidebar = () => {
                 <svg
                     viewBox="0 0 24 24"
                     fill="currentColor"
-                    height="2em"
+                    height="2.5em"
                     width="2.5em"
                     
                 >
