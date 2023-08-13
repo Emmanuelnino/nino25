ccfaef34105ff63b2b47615c5fff3e8361ee31ea
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-13 04:54:22 -0700

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,11 +24,11 @@ const Navbar = () => {
                 </div>
                 <div>
                     <div></div>
-                    <div className='md:hidden flex items-center gap-3'>
+                    <div className='md:flex hidden items-center gap-3'>
                         <a href="">Login</a>
                         <a href="" className='py-1 px-3 bg-blue-300 text-white hover:bg-blue-600 rounded transition duration-300'>Sign up</a>
                     </div>
-                    <div className='md:hidden '>
+                    <div className='w-5 h-2 right-0  md:hidden '>
                         <svg
                             viewBox="0 0 24 24"
                             fill="currentColor"
