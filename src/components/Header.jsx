77463644ffecf5093f754e2b0cbd264c933f85d3
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-14 00:36:57 -0700

    change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 
 const Navbar = () => {
-  // ... your existing Navbar code ...
+  
 };
 
 const HeroSection = () => {
   return (
-    <section className='min-h-screen justify-center items-center w-full flex bg-blue-600 text-white py-32'>
+    <section className='min-h-screen justify-center items-center w-full flex bg-[#fcaa4e] text-white py-32'>
       <div className='container mx-auto text-center'>
         <h1 className='text-4xl md:text-6xl font-bold mb-4'>
           Welcome to Your Favorite Website
@@ -14,7 +14,7 @@ const HeroSection = () => {
         <p className='text-lg md:text-xl mb-8'>
           Discover amazing features and explore our Projects.
         </p>
-        <a href="#" className='bg-white text-blue-600 py-2 px-6 rounded-full font-semibold text-lg hover:bg-blue-200 transition duration-300'>
+        <a href="#" className='bg-white text-[#fcaa4e] py-2 px-6 rounded-full font-semibold text-lg hover:bg-[#fcaa4e] transition duration-300'>
           Collaborate?
         </a>
       </div>
