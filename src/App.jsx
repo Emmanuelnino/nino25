d56b3d8060299e9467e287bab0cc03699b64ca66
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-13 21:03:21 -0700

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+import Features from './components/Features'
+import GetStartedPage from './components/Getstarted'
 
 const App = () => {
   return (
     <>
       <Navbar/>
       <Header/>
+      <Features/>
+      <GetStartedPage/>
     </>
   )
 }
