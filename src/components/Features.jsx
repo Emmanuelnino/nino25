78cebc4f20767315894998463b84fa6a66392f4d
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-16 00:55:41 -0700

    changes

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Features = () => {
   return (
-    <div></div>
+    <div>
+      <h1></h1>
+    </div>
   )
 }
 
--- src/components/Getstarted.jsx
+++ src/components/Getstarted.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const GetStartedPage = () => {
     return (
-        <section style={{backgroundImage:`https://images.pexels.com/photos/1510659/pexels-photo-1510659.jpeg?auto=compress&cs=tinysrgb&w=600`}} className=''>
-            <div className='bg-gray-100 min-h-screen flex justify-center items-center'>
-                <div className='max-w-md mx-auto p-8 bg-white shadow-xl rounded-xl'>
-                    <h2 className='text-2xl font-semibold mb-4'>Get Started</h2>
-                    <p className='text-gray-600 mb-6'>
+        <section style={{backgroundImage:`url(https://images.pexels.com/photos/1036936/pexels-photo-1036936.jpeg?auto=compress&cs=tinysrgb&w=600)`}} className='justify-center items-center h-full w-full bg-cover bg-center bg-no-repeat flex text-white'>
+            <div className='min-h-screen flex justify-center items-center'>
+                <div className='max-w-md mx-auto p-8 backdrop-blur-md bg-white/25 shadow-lg rounded-xl'>
+                    <h2 className='text-2xl font-bold mb-4'>Get Started</h2>
+                    <p className='text-white-600 font-bold mb-6'>
                         Choose a platform to get started with.
                     </p>
                     <div className='flex flex-col gap-4'>
@@ -18,7 +18,7 @@ const GetStartedPage = () => {
                         </a>
                         <div className=' flex flex-col gap-4'>
                             <a href="" className='py-2 px-4 bg-[#fcaa4e] text-white font-bold rounded-lg text-center bg-hover:opacity-90 transition transform duration-300'>Youtube</a>
-                            <a href="" className='py-2 px-4 bg-[#fcaa4e] text-white font-bold rounded-lg text-center bg-hover:opacity-90 transition transform duration-300'>LinkedIn</a>
+                            <a href="https://www.linkedin.com/in/emmanuel-tembo-132156260/" className='py-2 px-4 bg-[#fcaa4e] text-white font-bold rounded-lg text-center bg-hover:opacity-90 transition transform duration-300'>LinkedIn</a>
                         </div>
                         {/* Add more links/buttons as needed */}
                     </div>
