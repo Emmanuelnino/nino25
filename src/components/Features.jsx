b030630a1e5b9b9297b7c837504abd0f9295af21
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-18 03:22:21 -0700

    changes

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -20,9 +20,9 @@ const Features = () => {
             <svg
               viewBox="0 0 16 16"
               fill="currentColor"
-              height="1em"
-              width="1em"
-              
+              height="1.5em"
+              width="2.5em"
+
             >
               <path d="M10.28 6.28a.75.75 0 10-1.06-1.06L6.25 8.19l-.97-.97a.75.75 0 00-1.06 1.06l1.5 1.5a.75.75 0 001.06 0l3.5-3.5z" />
               <path
@@ -33,11 +33,26 @@ const Features = () => {
           </svg>
         </div>
 
-        <img
-          src="image2.jpg"
-          alt="Animation Example"
-          className="w-40 h-40 rounded-lg transition-transform hover:scale-110"
-        />
+        <div className="flex-1 px-96 flex justify-around">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="w-16 h-16 transition-transform hover:scale-110"
+          >
+            <svg
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              height="2.5em"
+              width="2.5em"
+              
+            >
+              <path d="M12 0a2.4 2.4 0 00-2.4 2.4A2.4 2.4 0 0012 4.8a2.4 2.4 0 002.4-2.4A2.4 2.4 0 0012 0zM9.543 9.543v4.914h4.914V9.543zM2.4 9.6A2.4 2.4 0 000 12a2.4 2.4 0 002.4 2.4A2.4 2.4 0 004.8 12a2.4 2.4 0 00-2.4-2.4zm19.2 0a2.4 2.4 0 00-2.4 2.4 2.4 2.4 0 002.4 2.4A2.4 2.4 0 0024 12a2.4 2.4 0 00-2.4-2.4zM12 19.2a2.4 2.4 0 00-2.4 2.4A2.4 2.4 0 0012 24a2.4 2.4 0 002.4-2.4 2.4 2.4 0 00-2.4-2.4z" />
+            </svg>
+          </svg>
+        </div>
+
       </div>
     </div>
   )
