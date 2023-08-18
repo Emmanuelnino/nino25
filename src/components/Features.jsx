f8a503fc935b3da7d4129e964e70a437c3e9d232
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-18 03:48:31 -0700

    changes

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
+import React from 'react';
 
 const Features = () => {
   return (
-    <div className="flex items-center justify-between bg-slate p-10 text-[#fcaa4e]">
-      <div className="flex-1">
-        <h2 className='px-10 font-bold text-4xl'> <span className='transition duration-300 transform hover:-translate-y-1'>Here</span> <span className=' transform hover:-translate-y-1'>we go!</span></h2>
-        <h2 className="py-96 text-3xl font-bold">Interactive Showcase</h2>
-        <p>Check out some of the cool things I can do!</p>
-      </div>
-      <div className="flex-1 px-96 flex justify-around">
-      <div className="flex-1 px-96 flex justify-around">
-  
-</div>
-
-
-        <div className="flex-1 px-96 flex justify-around">
-          
+    <div className="flex items-center justify-center h-screen text-[#fcaa4e]">
+      <div className="flex-1 text-center">
+        <h2 className="font-bold text-4xl mb-4">
+          <span className="text-[#fcaa4e] transition-transform hover:-translate-y-1 inline-block">
+            Here
+          </span>
+          <span className="text-[#fcaa4e] transition-transform hover:-translate-y-1 inline-block">
+            we go!
+          </span>
+        </h2>
+        <div className="mb-10">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="w-16 h-16 mx-auto animate-bounce"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M19 14l-7 7m0 0l-7-7m7 7V3"
+            />
+          </svg>
         </div>
-
+        <h2 className="text-[#fcaa4e] text-3xl font-bold mb-2">Interactive Showcase</h2>
+        <p className="text-[#fcaa4e] text-lg">
+          Explore some of the
+          <span className="text-[#fcaa4e]">awesome</span> things
+          I can do!
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Features
+export default Features;
