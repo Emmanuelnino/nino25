f7c0028be0f73666366858abb2b6b4560189fe81
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-18 02:26:43 -0700

    changes

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -4,6 +4,7 @@ const Features = () => {
   return (
     <div className="flex items-center justify-between bg-slate p-10 text-[#fcaa4e]">
       <div className="flex-1">
+        <h2 className='px-10 font-bold text-4xl'>Here we go!</h2>
         <h2 className="py-96 text-3xl font-bold">Interactive Showcase</h2>
         <p>Check out some of the cool things I can do!</p>
       </div>
