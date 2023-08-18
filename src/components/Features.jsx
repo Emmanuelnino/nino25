81720dbc91d804d2a2f8d987c11f2f687fc85d8a
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-18 01:42:08 -0700

    changes

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 const Features = () => {
   return (
-    <div>
-      <h1></h1>
+    <div className="flex items-center justify-between bg-slate p-10 text-white">
+      <div className="flex-1">
+        <h2 className="text-3xl font-bold">Interactive Showcase</h2>
+        <p>Check out some of the cool things I can do!</p>
+      </div>
+      <div className="flex-1 flex justify-around">
+        <img
+          src="image1.jpg"
+          alt="Animation Example"
+          className="w-40 h-40 rounded-lg transform transition-transform hover:scale-110"
+        />
+        <img
+          src="image2.jpg"
+          alt="Animation Example"
+          className="w-40 h-40 rounded-lg transform transition-transform hover:scale-110"
+        />
+      </div>
     </div>
   )
 }
